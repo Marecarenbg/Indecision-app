f505600cfaf817d7c1844e7d6996159026c8af0e
Marecarenbg <stajexexe@hotmail.com>
2018-02-13 16:10:56 +0100

    commiting

---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,26 @@
+var nameVar = "Andrew";
+var nameVar ="Mikee";
+console.log("nameVar", nameVar);
+
+let nameLet = 'Jen';
+nameLet = "Julie";
+console.log("nameLet", nameLet);
+
+const nameConst = "Frank";
+console.log("nameConst", nameConst);
+
+
+function getPetName (){
+	let petName = "hal";
+	return petName;
+}
+
+const fullName = 'Mare Care';
+let firstName;
+
+if(fullName) {
+	firstName = fullName.split(' ')[0];
+	console.log(firstName);
+}
+
+console.log(firstName);
